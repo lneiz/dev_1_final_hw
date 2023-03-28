a7c48e79904db1cb8f955c28c2daf30eb1abbd42
lneiz <yuntsevich@ymail.com>
2023-03-29 01:29:01 +0500

    input and data sellection

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
 ﻿
+void Main()
+{
+    Console.Write("Сколько элементов массива планируете ввести? ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите значения элементов массива построчно через Enter. Наберите 'end', чтобы завершить ввод: " + "\r\n");
+    string[] inputArray = new string[size];
+    string[] resultArray = new string[size];
+    int j = 0; int k = 0;
+    for(int i = 0; i < size; i++)
+    {
+        string inputLine = Console.ReadLine();
+        if(inputLine != "end")
+        {
+            inputArray[i] = inputLine;
+            j++;
+
+            if(inputArray[i].Length <= 3)
+            {
+                resultArray[k] = inputArray[i];
+                k++;
+            }
+        } 
+        else
+        {
+            while(i < size)
+            {
+                i++;
+            }
+        }
+    }
+    Array.Resize(ref inputArray, j);
+    Array.Resize(ref resultArray, k);
+
+    OutputArray(inputArray);
+    Console.Write(" -> ");
+    OutputArray(resultArray);
+}
